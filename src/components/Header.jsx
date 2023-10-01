360c30ee3bc15de4a89281535ca111320d104d3b
praththi <prathiunmansathiabalan@gmail.com>
2023-10-01 23:01:42 +0530

    fix nav bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 function Header() {
   return (
-    <nav className='bg-black text-white'>
+    <nav id="header" className='bg-black text-white'>
         <div className='w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2'>
-            <img src='/' alt='logo'/>
-        </div>
-        <div className='nav-menu-wrapper flex items-center justify-between space-x-10'>
-            <div>Home</div>
-            <div>About</div>
-        </div>
+          <div className='logo-wrapper pl-4 flex items-center'>
+            <h2>Foodie</h2>
+          </div>
 
+          <div className='nav-menu-wrapper flex items-center justify-between space-x-10'>
+              <div>Home</div>
+              <div>About</div>
+          </div>
+          <div className='flex items-center justify-center space-x-4'>
+            <div>Cart</div>
+            <div>Login</div>
+          </div>
+        </div>
     </nav>
   )
 }
