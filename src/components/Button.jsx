3b7e84e825ff846c28f2c4aea2ea840fbd8ca156
praththi <prathiunmansathiabalan@gmail.com>
2023-10-02 17:04:13 +0530

    add home page

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button({childern}) {
+  return (
+    <button>
+        {childern}
+    </button>
+  )
+}
+
+export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import cartIcon from '../assets/icons/cart.svg';
+import {Link} from 'react-router-dom';
 function Header() {
   return (
     <nav id="header" className='bg-black text-white'>
         <div className='w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2'>
           <div className='logo-wrapper pl-4 flex items-center'>
-            <h2>Foodie</h2>
+            <Link to="/" className='toggleColor text-white no-underline hover:no-underline font-bold text-xl lg:text-2xl'>
+              Foodie
+            </Link>
           </div>
 
           <div className='nav-menu-wrapper flex items-center justify-between space-x-10'>
-              <div>Home</div>
-              <div>About</div>
+              <Link to="/" className='text-xl'>Home</Link>
+              <Link to="#about" className='text-xl'>About</Link>
           </div>
           <div className='flex items-center justify-center space-x-4'>
-            <div>Cart</div>
-            <div>Login</div>
+            <Link to="/cart">
+              <img src={cartIcon} alt="cart"/>
+              </Link>
+            <Link to="/login">Login</Link>
+            <Link to="/logout">Logout</Link>
           </div>
         </div>
     </nav>
