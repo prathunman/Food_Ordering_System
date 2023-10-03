3efa68874ceb621c78c75ff4dff91ad97e9f6727
praththi <prathiunmansathiabalan@gmail.com>
2023-10-03 12:43:59 +0530

    set about componet

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='bg-white'>
+        <div className='p-24 grid grid-cols-2'>
+            <div className=''>
+                <h2 className='text-2xl font-medium'>About us</h2>
+                <p className='text-lg'>
+                Pizza Hut, a subsidiary of Yum! Brands, is the world's largest pizza company and home of Pan Pizza. Pizza Hut began 60 years ago in Wichita Kansas, and today is an iconic global brand that delivers more pizza, pasta and wings than any other restaurant in the world. Since its inception in 1993, Pizza Hut has fast become a household name across Sri Lanka. With its first restaurant at Union Place, Colombo 2, Pizza Hut became the first international restaurant chain to begin operations in Sri Lanka. Pizza Hut is a brand name belonging to the global restaurant chain Yum Brands Incorporated which is based in the USA. It is the world's largest restaurant company which also owns KFC & Taco Bell. In 1999, Pizza Hut ventured into the home delivery service setting yet another innovative milestone in Sri Lanka. 
+                </p>
+            </div>
+            <div className='flex items-center justify-center'>
+                <img src="" className='w-[400px] h-[400px] object-cover'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner';
+import About from '../components/About';
 
 function Home() {
   return (
     <div>
       <Banner/>
+      
+      <About/>
     </div>
   )
 }
