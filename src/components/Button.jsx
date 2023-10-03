16b5b2574b62e1cf30abdbd7b35bae33add4530f
praththi <prathiunmansathiabalan@gmail.com>
2023-10-03 08:03:55 +0530

    add style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import clsx from 'clsx';
 
-function Button({childern}) {
+function getClassName({ClassName}){
+    return clsx(
+        'text-white text-lg font-bold rounded-xl transition duration-300 cursor-pointer focus:outline-none focus:ring-2 focus:ring-opacity-50',ClassName
+    )
+}
+
+const sizes = {
+    small: 'px-4 py-3',
+    medium: 'px-6 py-4',
+    large: 'w-full px-4 py-3'
+};
+
+const variants = {
+    primary: 'bg-marigold focus:ring-marigold',
+    secondary: 'bg-tomato focus:ring-tomato',
+    dark: 'bg-black focus:ring-white'
+};
+
+function Button({childern,className,size='small',variant='primary',...rest}) {
   return (
-    <button>
+    <button className={clsx(sizes[size], variants[variant], getClassName({className}))}{...rest}>
         {childern}
     </button>
   )
