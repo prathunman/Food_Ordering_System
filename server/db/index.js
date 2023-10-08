a14c36658a66aafd077f9eb2ed2eb129d87ca502
praththi <prathiunmansathiabalan@gmail.com>
2023-10-08 10:11:04 +0530

    fix issue

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongo_url = "mongodb://localhost:27017";
+mongo_url = "mongodb://localhost:27017/Food_Order_System"; //to connect the Food_Order_System
 mongoose.connect(mongo_url,{useNewUrlParser: true}).catch(e=>{
     console.log('Connection error', e.message)
 })
