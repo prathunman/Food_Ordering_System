aa8b1fdc4198ff854280cd126b622e16c1b71378
praththi <prathiunmansathiabalan@gmail.com>
2023-10-08 10:10:38 +0530

    fix db config

--- server/db.config.js
+++ server/db.config.js
@@ -1,6 +1,6 @@
-const dbName = 'Food_Order_System';
-const dbHost='Localhost';
+const dbName = 'food-ordering';
+const dbHost = 'localhost';
 const dbPort = 27017;
-module.exports={
-    url:`mongodb://${dbHost}/${dbName}`
+module.exports = {
+    url: `mongodb://${dbHost}:${dbPort}/${dbName}`
 }
